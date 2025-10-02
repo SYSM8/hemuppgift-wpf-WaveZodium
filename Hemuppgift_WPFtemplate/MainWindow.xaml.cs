50ed24462fd039e3e56c9cf4e1a3b8d552b47929
WaveZodium <23120507+WaveZodium@users.noreply.github.com>
2025-10-02 13:29:28 +0200

    Added main window buttons, events and the other windows.

--- Hemuppgift_WPFtemplate/MainWindow.xaml.cs
+++ Hemuppgift_WPFtemplate/MainWindow.xaml.cs
@@ -1,16 +1,34 @@
 ﻿using System.Windows;
 
-namespace Hemuppgift_WPFtemplate
-{
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
-    {
-        //HEJ
-        public MainWindow()
-        {
-            InitializeComponent();
-        }
+namespace Hemuppgift_WPFtemplate;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window {
+    //HEJ
+    //Hej hej :)
+    public MainWindow() {
+        InitializeComponent();
+    }
+
+    private void btnGrid_Click(object sender, RoutedEventArgs e) {
+        GridWindow gridWindow = new GridWindow();
+        gridWindow.Show();
+    }
+
+    private void btnStackPanel_Click(object sender, RoutedEventArgs e) {
+        StackPanelWindow stackPanelWindow = new StackPanelWindow();
+        stackPanelWindow.Show();
+    }
+
+    private void btnWrapPanel_Click(object sender, RoutedEventArgs e) {
+        WrapPanelWindow wrapPanelWindow = new WrapPanelWindow();
+        wrapPanelWindow.Show();
+    }
+
+    private void btnDockPanel_Click(object sender, RoutedEventArgs e) {
+        DockPanelWindow dockPanelWindow = new DockPanelWindow();
+        dockPanelWindow.Show();
     }
 }
